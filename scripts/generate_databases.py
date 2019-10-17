714d8e4e659b38089dd9eb2ba236180c69125eac
LuK1337 <priv.luk@gmail.com>
2019-10-17 14:55:32 +0200

    Regenerate databases list

---
+++ scripts/generate_databases.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import lxml.html
+import urllib.request
+
+root = lxml.html.fromstring(urllib.request.urlopen('https://saucenao.com').read())
+
+databases = []
+
+for child in root.find('.//select[@name="database"]').getchildren():
+    text, value = child.text, int(child.get('value'))
+
+    if value == 999:
+        continue
+
+    databases.append((text, value))
+
+databases.insert(0, ('All databases', 999))
+
+v = [x[1] for x in databases]
+
+print('<string-array name="databases_entries">')
+
+for k in [x[0] for x in databases]:
+    print(f'    <item>{k}</item>')
+
+print('</string-array>')
+
+print('')
+
+print('<integer-array name="databases_values">')
+
+for v in [x[1] for x in databases]:
+    print(f'    <item>{v}</item>')
+
+print('</integer-array>')
