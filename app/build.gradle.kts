a6867dc82fd09f2956ce619080afe51aad1e98ab
LuK1337 <priv.luk@gmail.com>
2022-11-27 17:13:48 +0100

    Migrate gradle scripts to kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,87 @@
+import org.jetbrains.kotlin.config.KotlinCompilerVersion
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.luk.saucenao"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 18
+        versionName = "1.16"
+    }
+
+    buildTypes {
+        named("release") {
+            // Enables code shrinking, obfuscation, and optimization.
+            isMinifyEnabled = true
+
+            // Includes the default ProGuard rules files.
+            setProguardFiles(
+                listOf(
+                    getDefaultProguardFile("proguard-android.txt"),
+                    "proguard-rules.pro"
+                )
+            )
+        }
+    }
+
+    flavorDimensions += "version"
+
+    productFlavors {
+        create("github") {
+            dimension = "version"
+            buildConfigField("String", "SAUCENAO_HIDE", "\"0\"")
+        }
+
+        create("googlePlay") {
+            dimension = "version"
+            buildConfigField("String", "SAUCENAO_HIDE", "\"3\"")
+        }
+    }
+
+    compileOptions {
+        encoding = "UTF-8"
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.1"
+    }
+
+    lint {
+        disable += "GoogleAppIndexingWarning"
+    }
+}
+
+dependencies {
+    // Kotlin
+    implementation(kotlin("stdlib", KotlinCompilerVersion.VERSION))
+
+    // AndroidX
+    implementation("androidx.activity:activity-compose:1.6.1")
+    implementation("androidx.compose.foundation:foundation-layout:1.3.1")
+    implementation("androidx.compose.ui:ui:1.3.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.3.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.3.1")
+    implementation("androidx.compose.material3:material3:1.1.0-alpha02")
+
+    // Other
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.27.0")
+    implementation("io.coil-kt:coil-compose:2.2.2")
+    implementation("org.jsoup:jsoup:1.15.3")
+}
