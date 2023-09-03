dfd603361dc85bc46feb144667e4201be49b00db
srccrow <7214961+chaoscalm@users.noreply.github.com>
2023-09-03 16:04:57 +0200

    Upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.3.1"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
 
     lint {
@@ -69,7 +69,7 @@ android {
 
 dependencies {
     // Kotlin
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.10")
 
     // AndroidX
     implementation("androidx.activity:activity-compose:1.7.2")
