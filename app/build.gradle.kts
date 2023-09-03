75a80219b941f31fb55bebf245a9a5491fc30dc2
chaoscalm <7214961+chaoscalm@users.noreply.github.com>
2023-09-03 21:47:01 +0200

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,15 +70,15 @@ android {
 
 dependencies {
     // Kotlin
-    implementation(kotlin("stdlib", KotlinCompilerVersion.VERSION))
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.22"))
 
     // AndroidX
-    implementation("androidx.activity:activity-compose:1.6.1")
-    implementation("androidx.compose.foundation:foundation-layout:1.3.1")
-    implementation("androidx.compose.ui:ui:1.3.2")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.3.2")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.3.2")
-    implementation("androidx.compose.material3:material3:1.1.0-alpha03")
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.foundation:foundation-layout:1.5.0")
+    implementation("androidx.compose.ui:ui:1.5.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.5.0")
+    implementation("androidx.compose.material3:material3:1.1.1")
 
     // Other
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.27.0")
