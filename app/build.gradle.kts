6e3b62e3bf460d9049d19bb6928169df851f2979
chaoscalm <7214961+chaoscalm@users.noreply.github.com>
2023-09-03 22:02:17 +0200

    Target Android 14 SDK version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 }
 
 android {
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.luk.saucenao"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 20
         versionName = "1.18"
     }
