2aae9e92796a68aacb399261e5d97cc6640c1da3
Luca Stefani <luca.stefani.ge1@gmail.com>
2023-09-04 00:03:25 +0200

    Use LazyColumn

--- app/src/main/java/com/luk/saucenao/ui/screen/ResultsScreen.kt
+++ app/src/main/java/com/luk/saucenao/ui/screen/ResultsScreen.kt
@@ -1,12 +1,11 @@
 package com.luk.saucenao.ui.screen
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -21,15 +20,14 @@ import com.luk.saucenao.ui.mock.FakeResult
 @Composable
 fun ResultsScreen(results: ArrayList<Results.Result>, serverError: String? = null) {
     if (results.isNotEmpty()) {
-        Column(
+        LazyColumn(
             verticalArrangement = Arrangement.spacedBy(10.dp),
             modifier = Modifier
                 .fillMaxWidth()
                 .fillMaxHeight()
-                .padding(start = 10.dp, end = 10.dp)
-                .verticalScroll(rememberScrollState()),
+                .padding(start = 10.dp, end = 10.dp),
         ) {
-            results.forEach {
+            items(results) {
                 ResultCard(result = it)
             }
         }
