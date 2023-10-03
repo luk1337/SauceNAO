63684768ad9b2de865513a19cca42a0d8957a16b
LuK1337 <priv.luk@gmail.com>
2023-10-03 12:43:59 +0200

    Bump versionCode, versionName to [24, 1.22]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.luk.saucenao"
         minSdk = 21
         targetSdk = 34
-        versionCode = 23
-        versionName = "1.21"
+        versionCode = 24
+        versionName = "1.22"
     }
 
     buildTypes {
