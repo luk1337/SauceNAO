0fed479e0155cb0d4d8df8eaff37fc9708a3bb8c
LuK1337 <priv.luk@gmail.com>
2024-03-08 10:58:45 +0100

    Address Icons.Filled.ArrowBack deprecation warning

--- app/src/main/java/com/luk/saucenao/ui/screen/ResultsScreen.kt
+++ app/src/main/java/com/luk/saucenao/ui/screen/ResultsScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -47,7 +48,7 @@ fun ResultsScreen(
                             onClick = onBackPressed,
                         ) {
                             Icon(
-                                imageVector = Icons.Filled.ArrowBack,
+                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                                 contentDescription = null,
                             )
                         }
