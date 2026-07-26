f05162cd730901d92766fd82bdc24c89002201d5
LuK1337 <priv.luk@gmail.com>
2026-07-26 18:51:39 +0200

    Set `isAppearanceLight{Navigation,Status}Bars` to false

--- app/src/main/java/com/luk/saucenao/ui/theme/Theme.kt
+++ app/src/main/java/com/luk/saucenao/ui/theme/Theme.kt
@@ -1,20 +1,35 @@
 package com.luk.saucenao.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
 
 @Composable
 fun Theme(content: @Composable () -> Unit) {
     val context = LocalContext.current
+    val view = LocalView.current
+
+    if (!view.isInEditMode) {
+        SideEffect {
+            WindowCompat.getInsetsController((view.context as Activity).window, view).apply {
+                isAppearanceLightNavigationBars = false
+                isAppearanceLightStatusBars = false
+            }
+        }
+    }
+
     MaterialTheme(
         colorScheme = when {
             Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> dynamicDarkColorScheme(context)
