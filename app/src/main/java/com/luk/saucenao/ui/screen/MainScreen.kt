79368194751f2ac9845bdf8e1b8c392bd996b560
LuK1337 <priv.luk@gmail.com>
2026-07-26 18:59:09 +0200

    Fix TopAppBar background color

--- app/src/main/java/com/luk/saucenao/ui/screen/MainScreen.kt
+++ app/src/main/java/com/luk/saucenao/ui/screen/MainScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -148,7 +149,7 @@ fun MainScreen(
                     }
                 },
                 colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = colorResource(id = R.color.colorPrimary),
+                    containerColor = MaterialTheme.colorScheme.surfaceContainer,
                 ),
             )
         },
--- app/src/main/java/com/luk/saucenao/ui/screen/ResultsScreen.kt
+++ app/src/main/java/com/luk/saucenao/ui/screen/ResultsScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -53,7 +54,7 @@ fun ResultsScreen(
                         }
                     },
                     colors = TopAppBarDefaults.topAppBarColors(
-                        containerColor = colorResource(id = R.color.colorPrimary),
+                        containerColor = MaterialTheme.colorScheme.surfaceContainer,
                     )
                 )
             },
